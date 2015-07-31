95cc53d02f935445cc28be4757f88acc31939527
Tyler <josephtylerjones@gmail.com>
2015-07-30 17:20:48 -0700

    added visibility state

--- page-visibility.js
+++ page-visibility.js
@@ -1,21 +1,26 @@
 var Visibility = (function() {
 
   var hidden,
-      visibilitychange;
+      visibilitychange,
+      state;
 
   // Set the property and event names
   if (typeof document.hidden !== "undefined") {
     hidden = "hidden";
     visibilitychange = "visibilitychange";
+    state = "visibilityState";
   } else if (typeof document.mozHidden !== "undefined") {
     hidden = "mozHidden";
     visibilitychange = "mozvisibilitychange";
+    state = "mozVisibilityState";
   } else if (typeof document.msHidden !== "undefined") {
     hidden = "msHidden";
     visibilitychange = "msvisibilitychange";
+    state = "msVisibilityState";
   } else if (typeof document.webkitHidden !== "undefined") {
     hidden = "webkitHidden";
     visibilitychange = "webkitvisibilitychange";
+    state = "webkitVisibilityState";
   }
 
   /**
@@ -31,6 +36,7 @@ var Visibility = (function() {
 
   return {
     isHidden: isHidden,
+    state: state,
     hidden: hidden, // string name of the "hidden" property
     visibilitychange: visibilitychange // string name of the "visibilitychange" event
   };
